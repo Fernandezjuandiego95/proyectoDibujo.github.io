295b1526a12a341c5ca3426bedf80e6fa13124fb
Juan Diego Fernandez <fernandezgonzalezjuandiego95@gmail.com>
2022-05-09 17:35:44 -0500

    Trababjando en archivo javascript

---
+++ proceso.js
@@ -0,0 +1,9 @@
+/**
+ * Obtener el id de los elementos o etiquetas del html, que son nesesarias para el funcionanmiento del aplicactivo de dibujo
+ */
+
+var canva = document.getElementById("dibujar")
+var seleccionColor = document.getElementById("colorr")
+var seleccionGrosor = document.getElementById("grosorPluma")
+
+/*--------------------------------------------------------------------------------------------------------------------*/
